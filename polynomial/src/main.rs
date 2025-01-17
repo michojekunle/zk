60f45c1a332299c06f97b6cb067b97b8b0662e5c
AMD <michojekunle1@gmail.com>
2025-01-17 05:34:14 +0100

    implement lagrange interpolation with univariate polynomials

--- polynomial/src/main.rs
+++ polynomial/src/main.rs
@@ -1,7 +1,33 @@
-// sparse polynomial
+// sparse univariate polynomial
+#[derive(Clone, Debug)]
 struct UnivariatePoly {
     degree: i32,
     coefficients: Vec<(f64, i32)>,
+}
+
+// helper function to simplify tuples for composing tuples with
+// the same exponent and also simplifying adding polynomials function
+fn simplify_tuples(vec: Vec<(f64, i32)>) -> Vec<(f64, i32)> {
+    use std::collections::HashMap;
+
+    let mut map: HashMap<i32, f64> = HashMap::new();
+
+    // Group coefficients by exponent and sum them
+    for (coeff, exp) in vec {
+        *map.entry(exp).or_insert(0.0) += coeff;
+    }
+
+    // Convert the HashMap back into a vector of tuples, swapping (i32, f64) to (f64, i32)
+    let mut simplified: Vec<(f64, i32)> = map
+        .into_iter()
+        .filter(|&(_, c)| c != 0.0)
+        .map(|(exp, coeff)| (coeff, exp))
+        .collect();
+
+    // Sort the vector by exponent for consistency
+    simplified.sort_by_key(|&(_, exp)| std::cmp::Reverse(exp));
+
+    simplified
 }
 
 // in the form: yn . Ln(x)
@@ -9,30 +35,76 @@ fn lagrange_basis(point: (u32, u32), interpolating_poly: Vec<u32>) -> Univariate
     let (x, y) = point;
 
     let mut polys: Vec<UnivariatePoly> = Vec::new();
+    let mut int_prod: i32 = 1;
 
     for val in interpolating_poly {
         if val != x {
-            polys.push(UnivariatePoly::new(vec![(1.0, 1), (-(x as f64), 0)]));
+            polys.push(UnivariatePoly::new(vec![(1.0, 1), (-(val as f64), 0)]));
+            int_prod = int_prod * (x as i32 - val as i32);
         }
     }
 
-    let result = polys.iter().fold(UnivariatePoly::default(), |acc, &poly| multiply_poly(acc, poly));
+    println!();
+    println!("Integer product: {}", int_prod);
+    println!();
+
+    let result: UnivariatePoly = polys.iter().fold(UnivariatePoly::default(), |acc, poly| {
+        multiply_poly(acc, poly.clone())
+    });
 
     // get the integer/floating point part and multiply with the resultant poly
-    
+    let non_poly_co_eff: f64 = (y as f64 / int_prod as f64).into();
+
+    println!();
+    println!("y: {} \nFloating point product: {}", y, non_poly_co_eff);
+    println!();
+
+    let mut final_vec: Vec<(f64, i32)> = Vec::new();
+
+    for coefficient in &result.coefficients {
+        let (c, e) = coefficient;
+
+        final_vec.push((c * non_poly_co_eff, *e));
+    }
+
+    UnivariatePoly::new(final_vec)
 }
 
-fn multiply_poly(poly_a: UnivariatePoly, poly_b: UnivariatePoly) -> UnivariatePoly {}
+fn multiply_poly(poly_a: UnivariatePoly, poly_b: UnivariatePoly) -> UnivariatePoly {
+    let mut vec: Vec<(f64, i32)> = Vec::new();
+
+    for poly_a_coefficient in poly_a.coefficients {
+        let (c_a, e_a) = poly_a_coefficient;
+
+        for poly_b_coefficient in &poly_b.coefficients {
+            let (c_b, e_b) = poly_b_coefficient;
+            vec.push((c_a * c_b, e_a + e_b));
+        }
+    }
+
+    vec = simplify_tuples(vec);
+
+    UnivariatePoly::new(vec)
+}
 
 fn add_poly(poly_a: UnivariatePoly, poly_b: UnivariatePoly) -> UnivariatePoly {
+    let mut merged_polys: Vec<(f64, i32)> = poly_a
+        .coefficients
+        .iter()
+        .chain(poly_b.coefficients.iter())
+        .cloned()
+        .collect();
 
+    merged_polys = simplify_tuples(merged_polys);
+
+    UnivariatePoly::new(merged_polys)
 }
 
 impl UnivariatePoly {
     fn default() -> Self {
         UnivariatePoly {
             degree: 0,
-            coefficients: vec![(1.0, 0)], 
+            coefficients: vec![(1.0, 0)],
         }
     }
 
@@ -59,10 +131,16 @@ impl UnivariatePoly {
         let xs: Vec<u32> = eval.iter().map(|(x, _)| *x).collect();
         let mut sum: UnivariatePoly = UnivariatePoly::new(vec![(0.0, 0)]);
 
+        if self.degree < 1 {
+            return UnivariatePoly::new(vec![(0.0, 0)]);
+        }
+
         for point in eval {
             let curr: UnivariatePoly = lagrange_basis(point, xs.clone());
+            println!();
+            println!("Curr poly is: {:?}", curr.coefficients);
+            println!();
             sum = add_poly(sum, curr);
-            println!("x: {x}, y: {y}");
         }
 
         sum
@@ -94,28 +172,43 @@ fn main() {
     let poly_1_degree = poly_1.degree();
     let poly_1_eval_2 = poly_1.evaluate(2);
     let poly_1_eval_3 = poly_1.evaluate(3);
+    let poly_1_interpolated = poly_1.interpolate(vec![(2, 9), (3, 11)]);
 
     assert_eq!(poly_1_degree, 1);
     assert_eq!(poly_1_eval_2, 9);
     assert_eq!(poly_1_eval_3, 11);
+    assert_eq!(poly_1_interpolated.coefficients, vec![(2.0, 1), (5.0, 0)]);
 
     println!("Sparse Poly 1 degree: {}", poly_1_degree);
     println!("Sparse Poly 1 evaluated at 2: {}", poly_1_eval_2);
     println!("Sparse Poly 1 evaluated at 3: {}", poly_1_eval_3);
+    println!(
+        "Sparse Poly 1 interpolated given evaluation format: {:?}",
+        poly_1_interpolated.coefficients
+    );
     println!();
 
     let poly_2 = UnivariatePoly::new(vec![(3.0, 2), (2.0, 1), (5.0, 0)]);
     let poly_2_degree = poly_2.degree();
     let poly_2_eval_2 = poly_2.evaluate(2);
     let poly_2_eval_3 = poly_2.evaluate(3);
+    let poly_2_interpolated = poly_2.interpolate(vec![(2, 21), (3, 38), (1, 10)]);
 
     assert_eq!(poly_2_degree, 2);
     assert_eq!(poly_2_eval_2, 21);
     assert_eq!(poly_2_eval_3, 38);
+    assert_eq!(
+        poly_2_interpolated.coefficients,
+        vec![(3.0, 2), (2.0, 1), (5.0, 0)]
+    );
 
     println!("Sparse Poly 2 degree: {}", poly_2_degree);
     println!("Sparse Poly 2 evaluated at 2: {}", poly_2_eval_2);
     println!("Sparse Poly 2 evaluated at 3: {}", poly_2_eval_3);
+    println!(
+        "Sparse Poly 2 interpolated given evaluation format: {:?}",
+        poly_2_interpolated.coefficients
+    );
     println!();
 
     // test dense polynomial
@@ -162,18 +255,38 @@ mod tests {
     fn test_sparse_polynomials() {
         let poly = UnivariatePoly::new(vec![(1.0, 3), (2.0, 2), (3.0, 1), (4.0, 0)]);
         assert_eq!(poly.degree(), 3);
+        assert_eq!(poly.evaluate(1), 10);
         assert_eq!(poly.evaluate(2), 26);
         assert_eq!(poly.evaluate(3), 58);
+        assert_eq!(poly.evaluate(4), 112);
+        // Test interpolation with different points
+        assert_eq!(
+            poly.interpolate(vec![(2, 26), (3, 58), (1, 10), (4, 112)])
+                .coefficients,
+            vec![(1.0, 3), (2.0, 2), (3.0, 1), (4.0, 0)]
+        );
 
-        // Test zero polynomial
+        // Test zero polynomial interpolation
         let zero_poly = UnivariatePoly::new(vec![(0.0, 0)]);
         assert_eq!(zero_poly.degree(), 0);
         assert_eq!(zero_poly.evaluate(5), 0);
+        assert_eq!(
+            zero_poly
+                .interpolate(vec![(1, 0), (2, 0), (3, 0)])
+                .coefficients,
+            vec![(0.0, 0)]
+        );
 
         // Test single term polynomial
         let single_term = UnivariatePoly::new(vec![(3.0, 4)]);
         assert_eq!(single_term.degree(), 4);
         assert_eq!(single_term.evaluate(2), 48);
+        assert_eq!(
+            single_term
+                .interpolate(vec![(1, 3), (2, 48), (3, 243), (4, 768), (5, 1875)])
+                .coefficients,
+            vec![(3.0, 4)]
+        );
     }
 
     #[test]
