946b460dde8eab210d26fe61df540114cd87dfa9
AMD <michojekunle1@gmail.com>
2025-02-08 17:53:57 +0100

    .

--- gkr/src/circuit.rs
+++ gkr/src/circuit.rs
@@ -1,3 +1,6 @@
+use ark_ff::PrimeField;
+use crate::multilinear_poly::MultilinearPoly;
+
 struct Gate {
     left: usize,
     right: usize,
@@ -28,8 +31,8 @@ impl Gate {
         }
     }
 
-    fn eval() -> Self {
-        todo!()   
+    fn eval(&self) -> u64 {
+        
     }
 }
 
@@ -40,18 +43,29 @@ impl Layer {
 }
 
 impl <F: PrimeField>Circuit<F> {
-    fn new() -> Self {
+    fn new(layers: Vec<Layer>) -> Self {
+        Circuit {
+            layers
+        }
+    }
+
+    fn eval(&self, input_layer: Vec<F>) -> Vec<Vec<F>> {
 
     }
 
-    fn eval() -> F {
-
+    fn get_layer_poly(&self, layer_id: usize) -> MultilinearPoly {
+        todo!();
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_gate() {
+
+    }
 
     #[test]
     fn test_circuit_implementation() {
