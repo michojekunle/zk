4a6a464b249a4cd06ee256f1d6fdca2c1e601128
AMD <michojekunle1@gmail.com>
2025-02-20 17:13:23 +0100

    implementing

--- gkr/src/protocol.rs
+++ gkr/src/protocol.rs
@@ -1,1 +1,27 @@
-// use
+use crate::fiat_shamir::FiatShamir;
+use crate::multilinear_poly::MultilinearPoly;
+use ark_ff::{BigInteger, PrimeField};
+use sha3::Keccak256;
+use sumcheck::{PartialVerify, PartialProve};
+
+struct GKRProof {
+
+}
+
+pub (crate) fn GKRProve() {
+    todo!()
+}
+
+pub (crate) fn GKRVerify() {
+    todo!()
+}
+
+#[cfg(tests)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gkr_implementation() {
+        
+    }
+}
